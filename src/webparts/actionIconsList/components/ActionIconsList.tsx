fb295c544420d875bb85d8a22149c0548ebfc52e
Mostafa <mai.mostafa@peelsb.com>
2020-11-30 07:11:30 -0500

    restarting main app

--- src/webparts/actionIconsList/components/ActionIconsList.tsx
+++ src/webparts/actionIconsList/components/ActionIconsList.tsx
@@ -1,25 +1,22 @@
 import * as React from 'react';
 import styles from './ActionIconsList.module.scss';
 import { IActionIconsListProps } from './IActionIconsListProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+// import { escape } from '@microsoft/sp-lodash-subset';
 
-export default class ActionIconsList extends React.Component<IActionIconsListProps, {}> {
-  public render(): React.ReactElement<IActionIconsListProps> {
+import {DetailsList} from '@fluentui/react';
+
+import {CRUD} from '../Services/CRUD';
+
+export default function ActionIconsList (props:IActionIconsListProps) {
+
+    const crud = new CRUD();
+
+    React.useEffect(()=>{
+      console.log(crud.getAllLists(props.context));
+    })
+
     return (
-      <div className={ styles.actionIconsList }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div></div>
     );
-  }
+  
 }
